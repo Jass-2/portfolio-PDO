5657d5f16b1236c2c02b9d9eed5daa7ec2a50c70
Jass-2 <jashankumarcw@gmail.com>
2025-02-09 00:03:54 -0500

    updated the contact form
    In this commit I updated the contact form

--- sendmail.php
+++ sendmail.php
@@ -1,8 +1,6 @@
 <?php
-
 require_once('includes/connect.php');
 
-///gather the form content
 $fname = $_POST['fname'];
 $lname = $_POST['lname'];
 $email = $_POST['email'];
@@ -10,71 +8,56 @@ $msg = $_POST['message'];
 
 $errors = [];
 
-//validate and clean these values
-
 $fname = trim($fname);
 $lname = trim($lname);
 $email = trim($email);
 $msg = trim($msg);
 
-if(empty($lname)) {
-    $errors['lname'] = 'Last Name cant be empty';
+if (empty($fname)) {
+    $errors['name'] = 'Name cannot be empty';
 }
-
-if(empty($fname)) {
-    $errors['fname'] = 'First Name cant be empty';
+if (empty($lname)) {
+    $errors['number'] = 'Number cannot be empty';
 }
-
-if(empty($msg)) {
-    $errors['message'] = 'Comment field cant be empty';
+if (empty($msg)) {
+    $errors['message'] = 'Message field cannot be empty';
 }
-
-if(empty($email)) {
+if (empty($email)) {
     $errors['email'] = 'You must provide an email';
-} else if(!filter_var($email, FILTER_VALIDATE_EMAIL)) {
+} elseif (!filter_var($email, FILTER_VALIDATE_EMAIL)) {
     $errors['legit_email'] = 'You must provide a REAL email';
 }
 
-if(empty($errors)) {
+if (empty($errors)) {
+    try {
+        $query = "INSERT INTO contact (fname, lname, email, message) VALUES (?,?,?,?)";
+        $stmt = $connect->prepare($query);
+       
+        $stmt->bindParam(1, $fname, PDO::PARAM_STR);
+        $stmt->bindParam(2, $lname, PDO::PARAM_STR);
+        $stmt->bindParam(3, $email, PDO::PARAM_STR);
+        $stmt->bindParam(4, $msg, PDO::PARAM_STR);
 
-    //insert these values as a new row in the contacts table
+        if ($stmt->execute()) {
+            $to = 'jashankumar321@gmail.com';
+            $subject = 'Message from your Portfolio Website!';
+            $message = "You have received a new contact form submission:\n\n";
+            $message .= "Name: ".$fname." ".$lname."\n";
+            $message .= "Email: " . $email . "\n\n";
+            $message .= $msg;
 
-    $query = "INSERT INTO contact (fname,lname, email, message) VALUES(?,?,?,?)";
-
-    $stmt = $connect->prepare($query);
-
-    $stmt->bindParam(1, $fname, PDO::PARAM_STR);
-    $stmt->bindParam(2, $lname, PDO::PARAM_STR);
-    $stmt->bindParam(3, $email, PDO::PARAM_STR);
-    $stmt->bindParam(4, $msg, PDO::PARAM_STR);
-
-
-
-    if($stmt->execute()) {
-
-//format and send these values in an email
-
-        $to = 'rhaaf@fanshawec.ca';
-        $subject = 'Message from your Portfolio site!';
-
-        $message = "You have received a new contact form submission:\n\n";
-        $message .= "Name: ".$fname." ".$lname."\n";
-        $message .= "Email: ".$email."\n\n";
-//build out rest of message body...
-
-mail($to,$subject,$message);
-
-header('Location: /case-study.php');
-
-$stmt = null;
-} 
-
-
-
+            mail($to, $subject, $msg);
+            header('Location: thank_you.html');
+            exit;
+        } else {
+            echo "Database insertion failed!";
+        }
+    } catch (PDOException $e) {
+        echo "Database error: " . $e->getMessage();
+    }
 } else {
- for($i=0; $i < count($errors); $i++) {
-    echo $errors[$i].'<br>';
- }
+    foreach ($errors as $error) {
+        echo $error . '<br>';
+    }
 }
-
 ?>
